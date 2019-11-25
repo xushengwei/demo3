b34c2b2848eb357c2a752cc01c15019de9a85e7e
xushengwei <xsw20120203@sina.com>
2019-11-25 15:29:52 +0800

    commit

--- app/src/main/java/com/example/dm/MainActivity.java
+++ app/src/main/java/com/example/dm/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.dm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView = findViewById(R.id.tv);
+        textView.setText("hello hord");
 
     }
 }
